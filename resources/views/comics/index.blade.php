15870ff29ad215fbafb397193930a2758fe402db
DanieleFic <reb_ficus22@hotmail.it>
2022-03-10 18:54:27 +0100

    fatto un pò di css

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -3,9 +3,9 @@
 @section('title','Comics')
 
 @section('content')
-<h1>LISTA FUMETTI</h1>
+<h1 class="ciao">LISTA FUMETTI</h1>
 <a href="{{route("comics.create")}}"><button type="button" class="btn btn-info">Aggiungi Fumetto</button></a>
-<table class="table">
+{{-- <table class="table">
     <thead>
       <tr>
         <th scope="col">id</th>
@@ -32,7 +32,7 @@
             <td>{{$element->type}}</th>
             {{-- facciamo un bottone con il route comics.show che si trova nel nostro controllore  --}}
             {{-- <td><button href="{{route("comics.show", $element->id)}}" type="button" class="btn btn-info"></button></th> --}}
-            <td><a href="{{route("comics.show", $element->id)}}"><button type="button" class="btn btn-info">Informazioni</button></a></th>
+            {{-- <td><a href="{{route("comics.show", $element->id)}}"><button type="button" class="btn btn-info">Informazioni</button></a></th>
             <td><a href="{{route("comics.edit", $element->id)}}"><button type="button" class="btn btn-success">Modifica</button></a></th>
               <form action="{{route("comics.destroy", $element->id)}}" method="POST">
                 @csrf
@@ -40,8 +40,46 @@
                 <button type="submit" class="btn btn-danger" onclick="return confirm('Sicuro di cancellarlo?')">Elimina</button>
             </form>
         </tr>
-        @endforeach
-      
+        @endforeach 
     </tbody>
-  </table>
+  </table>--}}
+  <div class="container">
+    <ul class="responsive-table">
+      <li class="table-header">
+        {{-- <div class="col col-1">id</div> --}}
+        <div class="col col-1">Nome Fumetto</div>
+        <div class="col col-5">Descrizione</div>
+        <div class="col col-2">image</div>
+        <div class="col col-1">prezzo</div>
+        <div class="col col-1">Serie</div>
+        <div class="col col-1">data d'uscita</div>
+        <div class="col col-1">tipo</div>
+        {{-- <div class="col col-1">Modifiche</div> --}}
+      </li>
+      @foreach ($comics as $element)
+      <li class="table-row">
+        <div class="col col-1" data-label="Job Id">
+          {{$element->title}}
+          <form action="{{route("comics.destroy", $element->id)}}" method="POST">
+            @csrf
+            @method("DELETE")
+            <button type="submit" class="btn btn_delete" onclick="return confirm('Sicuro di cancellarlo?')">Elimina</button>
+        </form>
+        </div>
+        <div class="col col-5" data-label="Customer Name">{{$element->description}}</div>
+        <div class="col col-2" data-label="Amount"><img class="ms_img" src="{{$element->thumb}}" alt=""></div>
+        <div class="col col-1" data-label="Payment Status">{{$element->price}}</div>
+        <div class="col col-1">{{$element->series}}</div>
+        <div class="col col-1">{{$element->sale_date}}</div>
+        <div class="col col-1 ms_flex">
+          {{$element->type}}
+          <a href="{{route("comics.show", $element->id)}}"><button type="button" class="btn btn_info">Informazioni</button></a>
+          <a href="{{route("comics.edit", $element->id)}}"><button type="button" class="btn  btn_edit">Modifica</button></a>
+        </div>
+        
+      </li>
+      @endforeach
+    </ul>
+  </div>
+  
 @endsection
